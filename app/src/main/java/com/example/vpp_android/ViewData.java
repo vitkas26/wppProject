be4d86ae5e16069d3630318acb9d2f2a863039b8
Yuri Bembel <tdsainttds@gmail.com>
2021-05-05 17:30:05 +0600

    GetID costsItem not fixed

--- app/src/main/java/com/example/vpp_android/ViewData.java
+++ app/src/main/java/com/example/vpp_android/ViewData.java
@@ -2,18 +2,41 @@ package com.example.vpp_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.time.chrono.JapaneseChronology;
 import java.util.Objects;
 
-public class ViewData extends AppCompatActivity {
+public class ViewData extends AppCompatActivity{
+
+    Spinner costsItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_data);
+        costsItem = findViewById(R.id.costItems);
+
+        costsItem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 
@@ -30,4 +53,5 @@ public class ViewData extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
+
 }
